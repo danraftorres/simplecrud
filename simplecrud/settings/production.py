a4de22bc90ffbef2aab456ef5ba24491c7640da4
Daniel <dan.raf.torres@gmail.com>
2020-06-07 00:25:55 -0500

    Modified connection database production with DATABASE_URL

--- simplecrud/settings/production.py
+++ simplecrud/settings/production.py
@@ -7,12 +7,5 @@ MIDDLEWARE += []
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': env.str('DATABASE_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': env.str('DATABASE_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': env.str('DATABASE_USER', default=''),
-        'PASSWORD': env.str('DATABASE_PASSWORD', default=''),
-        'HOST': env.str('DATABASE_HOST', default=''),
-        'PORT': env.str('DATABASE_PORT', default='')
-    }
+    'default': env.db('DATABASE_URL', default='')
 }
